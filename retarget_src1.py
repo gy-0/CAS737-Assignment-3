b703f6469aaf2f77f5b89349fb8173094aaca969
Gao Yuan <gao131@mcmaster.ca>
2024-03-14 14:12:49 -0400

    Update retarget_src1.py
    update joint mapping

--- retarget_src1.py
+++ retarget_src1.py
@@ -19,6 +19,41 @@ dest_anim = dest_anim.repeat(len(src_anim) // len(dest_anim), axis=0)
 src_anim.positions[:, 0, :]+= dest_anim.positions[0, 0, :] - src_anim.positions[0, 0, :]
 dest_anim.positions[:,0,:] = src_anim.positions[:, 0, :]
 
+# dest:src
+joint_mapping = {
+    'Hips': 'Hips',
+    'LHipJoint': None,
+    'LeftUpLeg': 'LeftHip',
+    'LeftLeg': 'LeftKnee',
+    'LeftFoot': 'LeftAnkle',
+    'LeftToeBase': 'LeftToe',
+    'LowerBack': None,
+    'LeftShoulder': 'LeftCollar',
+    'LeftArm': 'LeftShoulder',
+    'LeftForeArm': 'LeftElbow',
+    'LeftHand': 'LeftWrist',
+    'LeftFingerBase': 'LeftFinger0',
+    'LeftHandIndex1': None,
+    'LThumb': None,
+    'Neck': 'Neck',
+    'Neck1': None,
+    'Head': 'Head',
+    'RHipJoint': None,
+    'RightUpLeg': 'RightHip',
+    'RightLeg': 'RightKnee',
+    'RightFoot': 'RightAnkle',
+    'RightToeBase': 'RightToe',
+    'RightShoulder': 'RightCollar',
+    'RightArm': 'RightShoulder',
+    'RightForeArm': 'RightElbow',
+    'RightHand': 'RightWrist',
+    'RightFingerBase': 'RightFinger0',
+    'RightHandIndex1': None,
+    'RThumb': None,
+    'Spine': 'Chest',
+    'Spine1': 'Chest2',
+}
+
 '''set goal positions
 goal_positions[j] are joint j's goal positions
 goal_positions[j] is of shape (F, 3), i.e. F number of frames, each frame is (x,y,z)
